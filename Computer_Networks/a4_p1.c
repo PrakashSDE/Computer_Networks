48ad5facccc7cbe5de68e6d7a10937624a66f6ba
Prakash Khatua <103178764+PrakashSDE@users.noreply.github.com>
2023-03-01 19:43:30 +0530

    Update a4_p1.c

--- Computer_Networks/a4_p1.c
+++ Computer_Networks/a4_p1.c
@@ -3,12 +3,15 @@
 main()
 {
         int pipefd[2],n;
-        char buff[100];
+
+                char buff[100];
         pipe(pipefd);
         printf("\nreadfd=%d",pipefd[0]);
         printf("\nwritefd=%d",pipefd[1]);
         write(pipefd[1],"helloworld",12);
-        n=read(pipefd[0],buff,sizeof(buff));
+        
+         n=read(pipefd[0],buff,sizeof(buff));
         printf("\nsize of data %d",n);
-        printf("\ndata from pipe: %s\n",buff);
+        printf("\ndata from pipe: %s\n",buff)
+       ;
 }
